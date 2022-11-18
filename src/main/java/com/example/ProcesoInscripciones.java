eff6ca36de61503acfb56833db27a910646155ee
Ram <ramdhei2002@gmail.com>
2022-11-18 15:24:17 -0500

    Clases Proceso y Main

---
+++ src/main/java/com/example/ProcesoInscripciones.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ProcesoInscripciones {
+    private HashMap<String, Estudiante> estudiantes;
+    private LectorArchivo lector;
+
+    public List<String> procesarArchivo(String ruta) {
+
+    }
+
+    private void procesarLinea(String linea) {
+
+    }
+
+    private Estudiante buscarEstudiante(String cedula) {
+
+    }
+}
